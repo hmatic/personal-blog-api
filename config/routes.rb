de59cfd56534b3db44a3d72eb66572fc419f50cf
Hrvoje Matic <web.hmatic@gmail.com>
2018-08-09 03:05:05 +0200

    Configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, except: [:new, :edit]
+      resources :posts, except: [:new, :edit]
+      resources :categories, except: [:new, :edit]
+      resources :tags, except: [:new, :edit]
+
+      resource :session, only: [:create, :destroy]
+    end
+  end
 end
